5e0b133de118d31fc50fd0fa67559ec629b7ed95
tech3br <dfsdireito95@gmail.com>
2022-04-09 00:53:47 -0300

    Insert new component Sidebar to main app

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenrerProps {
+  id: number;
+  title: string;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
 }
+
+interface SideBarProps {
+  title: string;
+  genres: Array<GenrerProps>;
+  selectedGenreId: number;
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export function SideBar({
+  title,
+  genres,
+  setSelectedGenreId,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>{title}</span>
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => setSelectedGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default SideBar;
